0ed688a964384040506ffe48489f56aa61ca4128
sharad-24 <500075287@stu.upes.ac.in>
2022-06-06 14:19:26 +0530

    structure ready

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+
+function Home() {
+    return (
+        <>
+           <h className="underline flex justify-center">welcome</h>
+        </>
+    );
+}
+
+export default withRouter(Home);
